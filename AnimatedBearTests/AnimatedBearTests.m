ef07e9e84a57fb077962bfedceeb51f9a2e02ca6
Jesus Magana <maganazero5@gmail.com>
2014-07-01 14:19:24 -0700

    Initial Commit

---
+++ AnimatedBearTests/AnimatedBearTests.m
@@ -0,0 +1,34 @@
+//
+//  AnimatedBearTests.m
+//  AnimatedBearTests
+//
+//  Created by Jesus Magana on 7/1/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AnimatedBearTests : XCTestCase
+
+@end
+
+@implementation AnimatedBearTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
